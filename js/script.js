8bf67c5570d918aab1af09f35c1585a17411faae
DANIE4588E <danielyongzx@gmail.com>
2025-08-03 23:05:37 +0800

    added simple backend

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 
+
+fetch('header.html')
+    .then(r => r.text())
+    .then(html => {
+        document.getElementById('header-placeholder').innerHTML = html;
+
+        const currentPath = window.location.pathname.split("/").pop();
+        const links = document.querySelectorAll('#header-placeholder a');
+
+        links.forEach(link => {
+            const linkPath = link.getAttribute('href');
+            if (linkPath === currentPath) {
+                link.classList.add('active');
+                link.addEventListener('click', e => e.preventDefault());
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    });
+
 fetch('footer.html')
     .then(r => r.text())
     .then(html => {
         document.getElementById('footer-placeholder').innerHTML = html;
     });
+
 document.addEventListener('DOMContentLoaded', () => {
     const els = document.querySelectorAll('.fade-in-element');
     const obs = new IntersectionObserver(
